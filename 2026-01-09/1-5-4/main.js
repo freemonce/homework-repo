a5e5a50e29b280db7d01cd248df1bab4734f44e9
freemonce <zestsettlesz@gmail.com>
2026-01-10 11:42:24 +0900

    2026-01-09 1-5-3 1-5-4 숙제 완료

---
+++ 2026-01-09/1-5-4/main.js
@@ -0,0 +1,7 @@
+import { add, sub } from "./math.js";
+import userProfile from "./userProfile.js";
+
+// 3. 실행 로직 (main.js에 유지)
+console.log(add(10, 20));
+console.log(sub(25, 10));
+console.log(userProfile.name);
---
+++ 2026-01-09/1-5-4/math.js
@@ -0,0 +1,8 @@
+// 1. 계산 함수들 -> [math.js]로 이동 예정
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
